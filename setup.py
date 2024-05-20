00af1da2a9c55e5175f51313783a894cc374bb0e
Michel Kohanim <michel@universal-devices.com>
2024-05-19 18:34:59 -0700

    v1.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ioxplugin',
-    version='1.1.2',
+    version='1.1.3',
     packages=find_packages(),
     description='IoX Plugin Helper Package',
     long_description=open('README.md').read(),

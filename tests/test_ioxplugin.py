50f7a0098e7e44dabc89746818ebb87615f3a7dc
Michel Kohanim <michel@universal-devices.com>
2024-07-12 16:24:55 -0700

    add store entry

--- tests/test_ioxplugin.py
+++ tests/test_ioxplugin.py
@@ -3,12 +3,17 @@
 #from ioxplugin import Plugin, ModbusIoX
 import os
 from ioxplugin import Plugin
+from ioxplugin import StoreEntry 
 
 curdir = os.curdir
 
 
+
 #plugin = Plugin("/usr/home/admin/workspace/plugin-dev/modbus/reference/modbus.iox_plugin.json")
 plugin = Plugin(f"{curdir}/tests/dimmer.iox_plugin.json")
+store=StoreEntry(plugin)
+store.addToStore("tech@universal-devices.com", "admin", "/usr/home/admin/workspace/ioxplugin/tests")
+
 s = plugin.areNodesStatic()
 plugin.toIoX()
 plugin.generateCode(path="/usr/home/admin/workspace/ioxplugin/tests")

ed8c44a981be143867ec61ede414c8c2e6bd3fc7
Michel Kohanim <michel@universal-devices.com>
2024-08-15 13:33:23 -0700

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ioxplugin',
-    version='1.2.1',
+    version='1.3.0',
     packages=find_packages(),
     description='IoX Plugin Helper Package',
     long_description=open('README.md').read(),

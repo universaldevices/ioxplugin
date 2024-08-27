e1dfb3802b33bd7424ec083a511f1609c5414eac
Michel Kohanim <michel@universal-devices.com>
2024-08-27 15:29:51 -0700

    support for installation/stop/start

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ioxplugin',
-    version='1.3.3',
+    version='1.3.4',
     packages=find_packages(),
     description='IoX Plugin Helper Package',
     long_description=open('README.md').read(),

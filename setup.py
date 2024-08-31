54e52f2ec1e12ec632a67d7b1c4404e2e581e32c
Michel Kohanim <michel@universal-devices.com>
2024-08-30 17:30:43 -0700

    fixed startup issues

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ioxplugin',
-    version='1.3.5',
+    version='1.3.6',
     packages=find_packages(),
     description='IoX Plugin Helper Package',
     long_description=open('README.md').read(),
